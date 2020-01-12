ad7b83eff0ead3008ae200103f82f09a68f56dd8
Roman Maru <romanmaru10@gmail.com>
2020-01-11 17:45:45 -0800

    Create Adding books to List

---
+++ app.js
@@ -0,0 +1,35 @@
+function Book(title, author, isbn) {
+  this.title = title
+  this.author = author
+  this.isbn = isbn
+}
+
+function UI() { }
+
+UI.prototype.addBookToList = book => {
+  const list = document.getElementById('book-list')
+
+  const row = document.createElement('tr')
+
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `
+  list.appendChild(row)
+}
+
+document.getElementById('book-form').addEventListener('submit', event => {
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value
+
+  const book = new Book(title, author, isbn)
+
+  const ui = new UI()
+
+  ui.addBookToList(book)
+
+  event.preventDefault()
+})
